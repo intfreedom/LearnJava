4387deafa341aa299a13a32eb350cfe6c9d72a2e
autonomy00001000 <48275378+autonomy00001000@users.noreply.github.com>
2019-03-07 11:04:09 +0800

    Java
    java

---
+++ Everyday/20190307/src/exceptionDemo/HelloWorld.java
@@ -0,0 +1,13 @@
+package exceptionDemo;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		int i = 0;
+		String greetings[] = {"Hello world!", "No, I mean it!",
+				"HELLO WORLD!!!"};
+		while(i < 4) {
+			System.out.println(greetings[i]);
+			i++;
+		}
+	}
+}

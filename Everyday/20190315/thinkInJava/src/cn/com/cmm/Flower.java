317ede85db177ea40a67132596d11400fd99bc43
intfreedom <>
2019-03-15 10:29:53 +0800

    1
    1

---
+++ Everyday/20190315/thinkInJava/src/cn/com/cmm/Flower.java
@@ -0,0 +1,34 @@
+package cn.com.cmm;
+
+public class Flower {
+	private int petalCount = 0;
+	private String s = new String("null");
+	Flower(int petals){
+		petalCount = petals;
+		System.out.println("Constructor w/ int arg only: " + petalCount);
+	}
+	Flower(String ss){
+		System.out.println("Constructor w/ String arg only, s="+ss);
+		s = ss;
+	}
+	Flower(String s, int petals){
+		this(petals);
+		this.s = s;
+		System.out.println("String & int args");
+	}
+	Flower(){
+		this("hi", 47);
+		System.out.println("default constructor (no args)");
+	}
+	
+	void print() {
+		System.out.println(
+				"petalConut = " + petalCount + "s = " + s);
+
+	}
+	public static void main(String[] args) {
+		Flower x = new Flower();
+		x.print();
+	}
+	
+}

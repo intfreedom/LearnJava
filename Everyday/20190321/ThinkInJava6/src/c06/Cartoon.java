85bbded99853dcd5a7ab274a0353ff6a661b10d4
intfreedom <36918526+intfreedom@users.noreply.github.com>
2019-03-21 16:45:00 +0800

    insist-Think in Java 6.5 Page150
    insist-Think in Java 6.5 Page150

---
+++ Everyday/20190321/ThinkInJava6/src/c06/Cartoon.java
@@ -0,0 +1,24 @@
+package c06;
+class Art{
+	Art(){
+		System.out.println("Art constructor");
+	}
+}
+
+class Drawing extends Art{
+	Drawing(){
+		System.out.println("Drawing constructor");
+	}
+}
+
+
+public class Cartoon extends Drawing {
+	Cartoon(){
+		System.out.println("Cartoon constructor");
+	}
+	public static void main(String[] args) {
+		Cartoon x = new Cartoon();
+	}
+	
+
+}

ebfbe57b7572ddbf1a9d6858d43e36736fc6464c
intfreedom <36918526+intfreedom@users.noreply.github.com>
2019-03-27 16:15:35 +0800

    Android chapter8

--- Android/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ Android/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -1,7 +1,6 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.content.Context;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -13,13 +12,19 @@ public class CrimeLab {
 
     public static CrimeLab get(Context context){
         if(sCrimeLab == null){
-            sCrimeLab == new CrimeLab(context);
+            sCrimeLab = new CrimeLab(context);
         }
         return sCrimeLab;
     }
 
     private  CrimeLab(Context context){
         mCrimes = new ArrayList<>();
+        for(int i = 0; i<100;i++){
+            Crime crime = new Crime();
+            crime.setTitle("Crime #"+i);
+            crime.setSolved(i % 2 == 0);
+            mCrimes.add(crime);
+        }
     }
 
     public List<Crime> getCrimes(){

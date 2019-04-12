7579c351d1e9fc8a83cba41127f74ade5809b8ad
intfreedom <36918526+intfreedom@users.noreply.github.com>
2019-04-12 10:17:49 +0800

    Android chapter 7

--- Android/BookDemo/07_UIFragments/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ Android/BookDemo/07_UIFragments/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -11,6 +11,7 @@ public class CrimeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //在setContentView()方法中调用onAttach(Context), onCreate(Bundle)和onCreateView(...)
         setContentView(R.layout.activity_crime);
         //Activity类中相应添加了FragmentManager类，负责管理fragment并将它们的视图添加到activity的视图层级结构中，
         //要以代码的方式将fragment添加给activity,需要直接调用activity的FragmentManager
@@ -28,5 +29,17 @@ public class CrimeActivity extends AppCompatActivity {
                 .add(R.id.fragment_container, fragment)
                 .commit();
         }
+        //以上代码总结：
+        //使用R.id.fragment_container的容器视图资源ID，向FragmentManager请求并获取fragment;
+        //activity被销毁时，它的FragmentManager会将队列保存下来；
+        //如果指定容器视图资源ID的fragment不存在，
+        //如果指定容器视图资源ID的fragment不存在，则fragment变量为空值，这时新建CrimeFragment，
+        // 并启动一个新的fragment事务；并将新建的Fragment添加到队列中；
+
+        //activity的FragmentManager负责调用队列中fragment的生命周期方法；添加fragment供FragmentManager管理时，
+        //onAttach(Context), onCreate(Bundle)和onCreateView(...)方法会被调用；
+
+        //托管activity的onCreate(Bundle)方法执行后，onActivityCreated(Bundle)方法也会被调用；
+        //因为是在CrimeActivity.onCreate(Bundle)方法中添加CrimeFragment，所以fragment被添加后，该方法会被调用；
     }
 }

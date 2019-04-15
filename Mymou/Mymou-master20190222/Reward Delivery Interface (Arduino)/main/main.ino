84b8dc66769fbcf17d105e7be4607bfc9096d51e
intfreedom <36918526+intfreedom@users.noreply.github.com>
2019-04-15 09:08:24 +0800

    1

--- Mymou/Mymou-master20190222/Reward Delivery Interface (Arduino)/main/main.ino
+++ Mymou/Mymou-master20190222/Reward Delivery Interface (Arduino)/main/main.ino
@@ -15,7 +15,8 @@ void setup() {
   pinMode(channelThree, OUTPUT);
   pinMode(channelFour, OUTPUT);
   pinMode(channelFive, OUTPUT);
-
+  //digitalWrite为Arduino中的一种函数，作用是设置引脚的输出电压为高电平或低电平，该函数无返回值，有两个参数pin和value。
+  //在使用digitalWrite(pin, value)函数之前要将引脚设置为OUTPUT模式。例：pinMode(x, OUTPUT);其中x为引脚编号。pinMode函数用于设置引脚模式。
   // Set all channels to off
   digitalWrite(channelTwo, LOW);
   digitalWrite(channelThree, LOW);
@@ -30,8 +31,9 @@ void setup() {
 
 void loop() {
   // Check for input
+  //串口通讯函数Serial.available()：判断串口缓冲区的状态，返回从串口缓冲区读取的字节数。
   if(Serial.available() > 0){
-
+	//串口通讯函数Serial.read()：读取串口数据，一次读一个字符，读完后删除已读数据
     bufferInt = Serial.read();
 
     if (bufferInt == '0') {

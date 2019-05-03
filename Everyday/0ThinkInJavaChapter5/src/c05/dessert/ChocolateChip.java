ae2795de15f4b009067b148a23446fe5dc00825d
intfreedom <liuwenjunxjtu@163.com>
2019-05-03 10:19:56 +0800

    insist 20190503

---
+++ Everyday/0ThinkInJavaChapter5/src/c05/dessert/ChocolateChip.java
@@ -0,0 +1,11 @@
+package c05.dessert;
+import c051.Cookie;
+
+public class ChocolateChip extends Cookie {
+	public ChocolateChip() {
+		System.out.println("ChocolateChip constructor");
+	}
+	public static void main(String[] args) {
+		ChocolateChip x = new ChocolateChip();
+	}
+}

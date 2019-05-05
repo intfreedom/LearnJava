c736ff225a85a3c59f6ec5cb8b56d513f30cb9ab
intfreedom <36918526+intfreedom@users.noreply.github.com>
2019-05-05 09:48:10 +0800

    insist extends

---
+++ Everyday/0ThinkInJavaChapter6/src/c06/Detergent.java
@@ -0,0 +1,34 @@
+package c06;
+
+class Cleanser{
+	private String s = new String("Cleanser");
+	public void append(String a) {s+=a;}
+	public void dilute() {append(" dilute()");}
+	public void apply() {append(" apply()");}
+	public void scrub() {append(" scrub()");}
+	public void print() {System.out.println(s);}
+	public static void main(String[] args) {
+		Cleanser x = new Cleanser();
+		x.dilute();
+		x.apply();
+		x.scrub();
+		x.print();
+	}
+}
+public class Detergent extends Cleanser {
+	public void scrub() {
+		append(" Detergent.scrub()");
+		super.scrub();
+	}
+	public void foam() {append(" foam()");}
+	public static void main(String[] args) {
+		Detergent x = new Detergent();
+		x.dilute();
+		x.apply();
+		x.scrub();
+		x.foam();
+		x.print();
+		System.out.println("Testing base class: ");
+		Cleanser.main(args);
+	}
+}

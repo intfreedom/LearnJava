575e42f345b360fb1517507320fecae31cbe799a
intfreedom <36918526+intfreedom@users.noreply.github.com>
2019-05-09 12:16:08 +0800

    insist

---
+++ Everyday/0ThinkInJavaChapter7/Chapter7/src/c07/Music.java
@@ -0,0 +1,44 @@
+package c07;
+
+class Note{
+	private int value;
+	private Note(int val) {
+		value = val;
+		System.out.println(value);
+	}
+	public static final Note 
+		middleC = new Note(0),
+		cSharp = new Note(1),
+		cFlat = new Note(2);
+//	public static final Note cFlat = new Note(2);
+}
+
+class Instrument{
+	public void play(Note n) {
+		System.out.println("Instrument.play()");
+	}
+}
+
+class Wind extends Instrument{
+	
+	public void play(Note n) {
+		System.out.println("Wind.play()");
+	}
+}
+
+
+public class Music {
+
+	public static void tune(Instrument i) {
+		System.out.println("whowhowho");
+		i.play(Note.middleC);
+		System.out.println("***********");
+	}
+	public static void main(String[] args) {
+		System.out.println("111111111111");
+		Wind flute = new Wind();
+		System.out.println("222222222222");
+		tune(flute);
+		System.out.println("3333333333333");
+	}
+}
---
+++ Everyday/0ThinkInJavaChapter7/Chapter7/src/c07/Music2.java
@@ -0,0 +1,73 @@
+package c07;
+
+class Note2{
+	private int value;
+	private Note2(int val) {value = val;}
+	public static final Note2
+		middleC = new Note2(0),
+		cSharp = new Note2(1),
+		cFlat = new Note2(2);
+}
+
+class Instrument2{
+	public void play(Note2 n) {
+		System.out.println("Instrument2.play()");
+	}
+}
+
+class Wind2 extends Instrument2{
+	public void play(Note2 n) {
+		System.out.println("Wind2.play()");
+	}
+}
+
+class Stringed2 extends Instrument2{
+	public void play(Note2 n) {
+		System.out.println("Stringed2.play()");
+	}
+}
+
+class Brass2 extends Instrument2{
+	public void play(Note2 n) {
+		System.out.println("Brass2.play()");
+	}
+}
+
+
+public class Music2 {
+
+	public static void tune(Wind2 i) {
+		i.play(Note2.middleC);
+	}
+	
+	public static void tune(Stringed2 i) {
+		i.play(Note2.middleC);
+	}
+	
+	public static void tune(Brass2 i) {
+		i.play(Note2.middleC);
+	}
+	
+	public static void tuneDiff(Instrument2 i) {
+		i.play(Note2.middleC);
+	}
+	
+	public static void main(String[] args) {
+		Wind2 flute  = new Wind2();
+		Stringed2 violin = new Stringed2();
+		Brass2 frenchHorn = new Brass2();
+		//ºı…Ÿ¥˙¬Î¡ø£ª
+		Instrument2 aaaaa = new Wind2();
+		Instrument2 bbbbb = new Stringed2();
+		Instrument2 ccccc = new Brass2();
+	
+		tuneDiff(aaaaa);
+		tuneDiff(bbbbb);
+		tuneDiff(ccccc);
+		System.out.println("***************");
+		tune(flute);
+		tune(violin);
+		tune(frenchHorn);
+	}
+	
+}
